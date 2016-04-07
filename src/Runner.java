302db99a2cbb144dfe0114ce5865811f0274b37b
ViktorFP <VikPr_85@mail.ru>
2016-04-07 12:11:45 +0300

    v 0.1

---
+++ src/Runner.java
@@ -0,0 +1,69 @@
+import by.epamlab.Constants;
+import by.epamlab.beans.Car;
+
+public class Runner {
+	private final static long DISQUALIFY_TIME = 5000;
+	private static long timeSleep = Long.MAX_VALUE;
+	private static String winner;
+
+	private static void start(Car... cars) {
+		for (Car car : cars) {
+			Thread thread = new Thread(car);
+			thread.start();
+			System.out.println("Car " + car.getName() + " started");
+		}
+		correctMinStep(cars);
+	}
+
+	private static boolean hasFinished(Car... cars) {
+		for (Car car : cars) {
+			Thread thread = new Thread(car);
+			if (!thread.isAlive()) {
+				winner = car.getName();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void correctMinStep(Car... cars) {
+		for (int i = 0; i < cars.length - 1; i++) {
+			long difference = Math.abs(cars[i].getFriction()
+					- cars[i + 1].getFriction());
+			if (difference < timeSleep) {
+				timeSleep = difference;
+			}
+		}
+	}
+
+	private static void disqualify(Car... cars) {
+		for (Car car : cars) {
+			Thread thread=new Thread(car);
+			if (thread.isAlive()) {
+				thread.interrupt();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// 1. Create cars with different parameters.
+		Car[] cars = { new Car("volvo_KolyaTeam", 200),
+				new Car("vas_VasyaTeam", 300), new Car("belas_CrashTeam", 500) };
+		// Start cars one by one.
+		start(cars);
+		// Observe which finishes first.
+		try {
+			while (!hasFinished(cars)) {
+				Thread.sleep(timeSleep);
+			}
+			// 2. Disqualify another cars
+			Thread.sleep(DISQUALIFY_TIME);
+			disqualify(cars);
+		} catch (InterruptedException e) {
+			System.out.println(Thread.currentThread().getName()
+					+ Constants.INTERRUPTED_EXCEPTION);
+		}
+		// 3. output the winner message
+		System.out.println("Winner is " + winner + "!");
+	}
+}
---
+++ src/by/epamlab/beans/Car.java
@@ -0,0 +1,47 @@
+package by.epamlab.beans;
+
+import by.epamlab.Constants;
+
+public class Car implements Runnable {
+	private static final long MAX_DISTANCE = 10000;
+	private static final long DISTANCE_STEP = 100;
+	private long friction;
+	private long distance;
+	private String name;
+
+	public Car(String name, long friction) {
+		this.name = name;
+		this.friction = friction;
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (distance < MAX_DISTANCE) {
+				Thread.sleep(friction);
+				distance += DISTANCE_STEP;
+			}
+			System.out.println(name + " finished");			
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			System.out.println(name + Constants.INTERRUPTED_EXCEPTION);
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getFriction() {
+		return friction;
+	}
+
+	public void setFriction(long friction) {
+		this.friction = friction;
+	}
+
+}
